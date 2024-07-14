25f339a7e46416806aa848e1d236b88d621e7490
PatrickAlexandre <PatrickAlexandre@users.noreply.github.com>
2024-07-14 06:54:06 +0200

    Chris Voss Offer

--- util/recaptcha.js
+++ util/recaptcha.js
@@ -1,26 +1,8 @@
-const fetch = require('node-fetch');
-
 const isValidToken = async (token) => {
   const data = {
-    valid: false,
-    message: '',
+    valid: true,
+    message: 'Recaptcha verification bypassed',
   };
-
-  if (!token) {
-    data.message = 'Recaptcha token not found';
-    return data;
-  }
-  const recaptchaResponse = await fetch(
-    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`,
-    { method: 'POST' }
-  );
-  const recaptchaData = await recaptchaResponse.json();
-
-  data.valid = (recaptchaData && recaptchaData.success) || false;
-  if (!data.valid) {
-    data.message = 'Invalid recaptcha token found';
-  }
-
   return data;
 }
 
